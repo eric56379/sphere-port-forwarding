f2f43bd700d15793425abb863560e7b6d2b9796b
Eric Nieters <60331611+eric56379@users.noreply.github.com>
2023-10-26 17:54:22 -0500

    Fixed a bug in port-forward-mac.sh.

--- port-forward-mac.sh
+++ port-forward-mac.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+user=$SUDO_USER
+
 if [ "$EUID" -ne 0 ]
     then echo "Script is being ran without sudo. Exiting..."
     exit 1
@@ -17,11 +19,13 @@ if [ ! -d ~/.ssh/ ]; then
 fi
 
 echo "Extracting the tarball."
-# tar -xf port-forward.tar.gz
+tar -xf port-forward.tar.gz
 if [[ $? -ne 0 ]]; then 
     echo "Error extracting the tarball."
     exit 1
 fi
+
+cd port-forwarding-data/
 
 if [ ! -f config ]; then
     echo "config was not found in the tarball. Please re-run the setup from your XDC and try again."
@@ -36,11 +40,14 @@ fi
 echo "Checking for config file..."
 if [ ! -e ~/.ssh/config ]; then
     echo "config does not exist. Moving current config file from tarball."
-    mv config ~/.ssh/
+    touch ~/.ssh/config
+    cat config >> $HOME/.ssh/config
+    # mv config ~/.ssh/
 else 
     echo "File exists."
     if grep -q "Host mergejump" "$HOME/.ssh/config"; then
         echo "Merge information was found. Ignoring..."
+        rm -f config
     else
         echo "Merge information was not found. Appending the config file to the existing config file..."
         echo -e "" >> $HOME/.ssh/config
@@ -51,7 +58,9 @@ fi
 
 echo "Moving merge_key into ~/.ssh..."
 mv merge_key ~/.ssh/
-chmod 600 ~/.ssh/merge_key
+cd ~/.ssh
+chmod 600 merge_key
+chown $user merge_key config
 
 echo -e "\nTask completed. You may now delete this tarball."
 echo "HINT: Before deleting the tarball, view the README.md file for instructions with how to access your XDC from terminal."
