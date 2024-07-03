62fc4fff5408b564ce9386e2e76f9b138d79f5be
Eric Nieters <60331611+eric56379@users.noreply.github.com>
2024-07-03 11:43:48 -0500

    Update port-forward-windows.ps1

--- port-forward-windows.ps1
+++ port-forward-windows.ps1
@@ -1,59 +1,62 @@
 Write-Host "Checking if $HOME\.ssh\ exists."
-if (Test-Path $HOME\.ssh\) {
+if (Test-Path "$HOME\.ssh\") {
     Write-Host "$HOME\.ssh\ exists."
 } else {
     Write-Host "$HOME\.ssh\ does not exist. Creating a new folder..."
-    New-Item -ItemType Directory -Path $HOME\.ssh\
+    New-Item -ItemType Directory -Path "$HOME\.ssh\"
 }
 
-if (Test-Path config) {
+$currentDir = Get-Location
+$parentDir = Split-Path -Path $currentDir -Parent
+
+if (Test-Path -Path "$parentDir\config") {
+    Write-Host "config found in the parent directory."
     # Continue.
 } else {
-    Write-Host "config was not found in the zip file. Please re-run the setup from your XDC and try again."
+    Write-Host "config was not found in the parent directory. Please re-run the setup from your XDC and try again."
     exit
 }
 
-if (Test-Path merge_key) {
+if (Test-Path -Path "$parentDir\merge_key") {
+    Write-Host "merge_key found in the parent directory."
     # Continue.
 } else {
-    Write-Host "merge_key was not found in the zip file. Please re-run the setup from your XDC and try again."
+    Write-Host "merge_key was not found in the parent directory. Please re-run the setup from your XDC and try again."
     exit
 }
 
 Write-Host "Checking for config file..."
 
-if (-not(Test-Path $HOME\.ssh\config)) {
+if (-not(Test-Path "$HOME\.ssh\config")) {
     Write-Host "$HOME\.ssh\config does not exist. Creating the config file..."
-    Copy-Item config -Destination "$HOME\.ssh\"
+    Copy-Item "$parentDir\config" -Destination "$HOME\.ssh\"
 } else {
     Write-Host "$HOME\.ssh\config exists."
 
-    if (Select-String -Path $HOME\.ssh\config -Pattern "Host mergejump") {
+    if (Select-String -Path "$HOME\.ssh\config" -Pattern "Host mergejump") {
         Write-Host "Merge data already exists in the config file. Ignoring."
-    }
-
-    else {
+    } else {
         Write-Host "Merge data does not already exist in the config file. Appending..."
         # Adding a new line before appending the content from config into the config in \.ssh.
-        Add-Content -Path $HOME\.ssh\config -Value "`n"
-        Get-Content -Path config | Add-Content -Path $HOME\.ssh\config
+        Add-Content -Path "$HOME\.ssh\config" -Value "`n"
+        Get-Content -Path "$parentDir\config" | Add-Content -Path "$HOME\.ssh\config"
     }
 }
 
 Write-Host "Checking if known_hosts exists..."
-if (-not(Test-Path $HOME\.ssh\known_hosts)) {
+if (-not(Test-Path "$HOME\.ssh\known_hosts")) {
     Write-Host "$HOME\.ssh\known_hosts does not exist. Creating the known_hosts file..."
-    New-Item $HOME\.ssh\known_hosts -type file
+    New-Item "$HOME\.ssh\known_hosts" -type file
 } else {
     Write-Host "known_hosts exists. Ignoring..."
 }
 
 Write-Host "Moving merge_key into $HOME\.ssh..."
-if ((Test-Path $HOME\.ssh\merge_key)) {
+if (Test-Path "$HOME\.ssh\merge_key") {
     Write-Host "merge_key already exists. Ignoring."
     Write-Host "If you want to update it, please delete it from $HOME\.ssh and re-run this script."
 } else {
-    Move-Item -Path merge_key -Destination $HOME\.ssh
+    Move-Item -Path "$parentDir\merge_key" -Destination "$HOME\.ssh\"
 }
 
 Write-Host "`n"
