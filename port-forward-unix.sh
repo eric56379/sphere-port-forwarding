4f11f83edf87e81cab9d28ca98a4e6b73813a510
Eric Nieters <60331611+eric56379@users.noreply.github.com>
2024-09-30 14:07:52 -0500

    Update port-forward-unix.sh

--- port-forward-unix.sh
+++ port-forward-unix.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 USER_HOME=$HOME
+CONFIG="../config"
+KEY="../merge_key"
 
 echo "Checking for $USER_HOME/.ssh/ folder..."
 if [ ! -d "$USER_HOME/.ssh/" ]; then
@@ -13,12 +15,12 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-if [ ! -f config ]; then
+if [ ! -f $CONFIG ]; then
     echo "config was not found in the zip file. Please re-run the setup from your XDC and try again."
     exit 1
 fi
 
-if [ ! -f merge_key ]; then
+if [ ! -f ../merge_key ]; then
     echo "merge_key was not found in the zip file. Please re-run the setup from your XDC and try again."
     exit 1
 fi
@@ -27,7 +29,7 @@ echo "Checking for config file..."
 if [ ! -f "$USER_HOME/.ssh/config" ]; then
     echo "config does not exist. Creating the config file."
     touch $USER_HOME/.ssh/config
-    cat config >> $USER_HOME/.ssh/config
+    cat $CONFIG >> $USER_HOME/.ssh/config
 else 
     echo "File exists."
     if grep -Fxq "Host mergejump" "$USER_HOME/.ssh/config"; then
@@ -35,7 +37,7 @@ else
     else
         echo "Merge information was not found. Appending the config file to the existing config file..."
         echo -e "" >> $USER_HOME/.ssh/config
-        cat config >> $USER_HOME/.ssh/config
+        cat $CONFIG >> $USER_HOME/.ssh/config
     fi
 fi 
 
@@ -50,7 +52,7 @@ else
 fi 
 
 echo "Moving merge_key into $USER_HOME/.ssh..."
-mv merge_key $USER_HOME/.ssh/
+mv $KEY $USER_HOME/.ssh/
 pushd $USER_HOME/.ssh > /dev/null
 chmod 600 merge_key
 chown $USER merge_key config
